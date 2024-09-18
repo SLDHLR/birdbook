7957c9357367b54949a43f7ab01cba45e5e6b709
Damith Harshana <damith.dhlr@hotmail.com>
2024-09-18 21:29:24 +0530

    Added all files

---
+++ docs/public/js/script.js
@@ -0,0 +1,18 @@
+let text = document.getElementById('text');
+let leaf = document.getElementById('leaf');
+let hill1 = document.getElementById('hill1');
+let hill4 = document.getElementById('hill4');
+let hill5 = document.getElementById('hill5');
+
+let maxScroll = 1000; // Adjust this to the maximum scroll value you want
+
+window.addEventListener('scroll', () => {
+    let value = Math.min(window.scrollY, maxScroll); // Restrict scrolling value to maxScroll
+
+    text.style.marginTop = value * 2.5 + 'px';
+    leaf.style.top = value * -1.5 + 'px';
+    leaf.style.left = value * 1.5 + 'px';
+    hill5.style.left = value * 1.5 + 'px';
+    hill4.style.left = value * -1.5 + 'px';
+    hill1.style.top = value * 1 + 'px';
+});
